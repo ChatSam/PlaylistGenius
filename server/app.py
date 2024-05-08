e5d980732c0d186687aa04f97cfc7e4df6b8fbdc
nowfuture011130 <105948127+nowfuture011130@users.noreply.github.com>
2024-05-08 12:23:46 -0400

    Update app.py

--- server/app.py
+++ server/app.py
@@ -41,5 +41,6 @@ def exchange_code():
         print(response.json())
         return jsonify({"error": "Failed to retrieve access token"}), 400
 
+
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, port=5000)
